b9933a25d38793723871bcee74fec44de1ea692e
jamsidedown <robertanderson1993@googlemail.com>
2020-10-01 23:15:05 +0100

    Felt refactored, might delete later

--- aws_dynamodb_parser/utils.py
+++ aws_dynamodb_parser/utils.py
@@ -6,45 +6,47 @@ def parse(dynamodb_object: Union[dict, list]) -> Union[dict, list]:
     if isinstance(dynamodb_object, list):
         return [parse(obj) for obj in dynamodb_object]
 
-    parsed = {}
-    for key in dynamodb_object.keys():
-        parsed[key] = _parse_pair(dynamodb_object[key])
-    return parsed
+    return {key: _parse_property(value) for key, value in dynamodb_object.items()}
 
 
 # pylint:disable=too-many-return-statements
-def _parse_pair(type_value_pair: dict) -> Any:
-    data_type = list(type_value_pair.keys())[0]
+def _parse_property(prop: dict) -> Any:
+    data_type = next(iter(prop))
+    value = prop.get(data_type, [])
 
     if data_type in {"S", "SS", "BOOL"}:
-        return type_value_pair[data_type]
+        return value
 
     if data_type == "N":
-        return _to_num(type_value_pair[data_type])
+        return _to_num(value)
 
     if data_type == "NS":
-        return [_to_num(data) for data in type_value_pair[data_type]]
+        return [_to_num(item) for item in value]
 
     if data_type == "B":
-        return base64.decodebytes(bytes(type_value_pair[data_type], "utf-8"))
+        return _to_bytes(value)
 
     if data_type == "BS":
-        return [base64.decodebytes(bytes(data, "utf-8")) for data in type_value_pair[data_type]]
+        return [_to_bytes(item) for item in value]
 
     if data_type == "M":
-        return parse(type_value_pair[data_type])
+        return parse(value)
 
     if data_type == "L":
-        return [_parse_pair(item) for item in type_value_pair[data_type]]
+        return [_parse_property(item) for item in value]
 
-    if data_type == "NULL" and type_value_pair[data_type]:
+    if data_type == "NULL" and value:
         return None
 
-    raise TypeError("Unknown DynamoDB data type '%s' with value '%s'" % (data_type, type_value_pair[data_type]))
+    raise TypeError("Unknown DynamoDB data type '%s' with value '%s'" % (data_type, value))
 
 
-def _to_num(number: Any) -> Union[float, int]:
+def _to_num(value: Any) -> Union[float, int]:
     try:
-        return int(number)
+        return int(value)
     except ValueError:
-        return float(number)
+        return float(value)
+
+
+def _to_bytes(value: Any) -> bytes:
+    return base64.decodebytes(bytes(value, "utf-8"))
